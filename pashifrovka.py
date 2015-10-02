c8f749afd38fb6ad48a42dc79501c6323e73fec0
unknown <shamurzaev_a@12-07.iuca.loc>
2015-10-02 11:04:33 +0600

    krutaya

---
+++ pashifrovka.py
@@ -0,0 +1,21 @@
+print "vvedite parol"
+s=raw_input(">>>")
+for char in s:
+	if char =="a":
+	    char = "b"
+    elif char =="b":
+        char ="c"
+    elif char =="c":
+        char = "d"
+    elif char == "d":
+        char = "e"
+    elif char == "e":
+        char = "f"
+    elif char == "f":
+        char = "g"
+    elif char == "g":
+        char = "h"
+    elif char == "!":
+        char = "h"
+    print char
+
---
+++ zamena.py
@@ -0,0 +1,7 @@
+s="5 878 88 9 6 apple4 52"
+news=""
+for n in s:
+	if n =="":
+		n="!"
+	news+=n
+print news
